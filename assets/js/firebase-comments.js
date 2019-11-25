d4895f86ae688fd8c66ae31c5f2099527bc3da18
speedetail <56748033+speedetail@users.noreply.github.com>
2019-11-26 00:45:51 +0900

    Update firebase-comments.js

--- assets/js/firebase-comments.js
+++ assets/js/firebase-comments.js
@@ -4,32 +4,33 @@
 //     //var link = $("link[rel='canonical']").attr("href");
 //     //var pathkey = decodeURI(link.replace(new RegExp('\\/|\\.', 'g'),"_"));
 //     //var postRef = postComments.child(pathkey);
-//     //var rootRef = firebase.database().ref();
+    var rootRef = firebase.database().ref();
 //     var rootRef = new Firebase("https://speedetail-web.firebaseio.com/")
-//     var postRef = rootRef.child(slugify(window.location.pathname));
-//     $("#comment").submit(function() {
-//         postRef.push().set({
-//             name: $("#name").val(),
-//             message: $("#message").val(),
-//             md5Email: md5($("#email").val()),
-//             postedAt: firebase.database.ServerValue.TIMESTAMP
-//         });
-//         $("input[type=text], textarea").val("");
-//         return false;
-//     });    
+    var postRef = rootRef.child(slugify(window.location.pathname));
+    $("#comment").submit(function() {
+        postRef.push().set({
+            name: $("#name").val(),
+            message: $("#message").val(),
+            md5Email: md5($("#email").val()),
+            postedAt: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("input[type=text], textarea").val("");
+        return false;
+    });    
   
-// //     postRef.on("child_added", function(snapshot) {
-// //         var newComment = snapshot.val();
-// //         var converter = new showdown.Converter({ extensions: ['xssfilter'] });
-// //         converter.setFlavor('github');
-// //         var markedMessage = converter.makeHtml(newComment.message);
-// //         var html = "<div class='comment'>";
-// //         html += "<h4>" + newComment.name + "</h4>";
-// //         html += "<div class='profile-image'><img src='https://www.gravatar.com/avatar/" + newComment.md5Email + "?s=100&d=retro'/></div>";
-// //         html += "<span class='date'>" + jQuery.timeago(newComment.postedAt) + "</span>"
-// //         html += "<p>" + markedMessage  + "</p></div>";
-// //         $("#comments-container").prepend(html);
-// //     });
+    postRef.on("child_added", function(snapshot) {
+        var newComment = snapshot.val();
+        var converter = new showdown.Converter({ extensions: ['xssfilter'] });
+        converter.setFlavor('github');
+        var markedMessage = converter.makeHtml(newComment.message);
+        var html = "<div class='comment'>";
+        html += "<h4>" + newComment.name + "</h4>";
+        html += "<div class='profile-image'><img src='https://www.gravatar.com/avatar/" + newComment.md5Email + "?s=100&d=retro'/></div>";
+        html += "<span class='date'>" + jQuery.timeago(newComment.postedAt) + "</span>"
+        html += "<p>" + markedMessage  + "</p></div>";
+        $(".comments").prepend(html);
+    });
+
 //     postRef.on("child_added", function(snapshot) {
 //       var newPost = snapshot.val();
 //       $(".comments").prepend('<div class="comment">' +
@@ -38,17 +39,17 @@
 //         '<span class="date">' + moment(newPost.postedAt).fromNow() + '</span><p>' + escapeHtml(newPost.message)  + '</p></div>');
 //     });
 
-// function slugify(text) {
-//   return text.toString().toLowerCase().trim()
-//     .replace(/&/g, '-and-')
-//     .replace(/[\s\W-]+/g, '-')
-//     .replace(/[^a-zA-Z0-9-_]+/g,'');
-// }
+function slugify(text) {
+  return text.toString().toLowerCase().trim()
+    .replace(/&/g, '-and-')
+    .replace(/[\s\W-]+/g, '-')
+    .replace(/[^a-zA-Z0-9-_]+/g,'');
+}
 
 
-// function escapeHtml(str) {
-//     var div = document.createElement('div');
-//     div.appendChild(document.createTextNode(str));
-//     return div.innerHTML;
-// }
-$(function(){var ref=new Firebase("https://speedetail-web.firebaseio.com/"),postRef=ref.child(slugify(window.location.pathname));postRef.on("child_added",function(snapshot){var newPost=snapshot.val();$(".comments").prepend('<div class="comment">'+"<h4>"+escapeHtml(newPost.name)+"</h4>"+'<div class="profile-image"><img src="http://www.gravatar.com/avatar/'+escapeHtml(newPost.md5Email)+'?s=100&d=retro"/></div> '+'<span class="date">'+moment(newPost.postedAt).fromNow()+"</span><p>"+escapeHtml(newPost.message)+"</p></div>")});$("#comment").submit(function(){var a=postRef.push();console.log(a);a.set({name:$("#name").val(),message:$("#message").val(),md5Email:md5($("#email").val()),postedAt:Firebase.ServerValue.TIMESTAMP});$("input[type=text], textarea").val("");return false})});function slugify(text){return text.toString().toLowerCase().trim().replace(/&/g,"-and-").replace(/[\s\W-]+/g,"-").replace(/[^a-zA-Z0-9-_]+/g,"")}function escapeHtml(str){var div=document.createElement("div");div.appendChild(document.createTextNode(str));return div.innerHTML}
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+     return div.innerHTML;
+ }
+//$(function(){var ref=new Firebase("https://speedetail-web.firebaseio.com/"),postRef=ref.child(slugify(window.location.pathname));postRef.on("child_added",function(snapshot){var newPost=snapshot.val();$(".comments").prepend('<div class="comment">'+"<h4>"+escapeHtml(newPost.name)+"</h4>"+'<div class="profile-image"><img src="http://www.gravatar.com/avatar/'+escapeHtml(newPost.md5Email)+'?s=100&d=retro"/></div> '+'<span class="date">'+moment(newPost.postedAt).fromNow()+"</span><p>"+escapeHtml(newPost.message)+"</p></div>")});$("#comment").submit(function(){var a=postRef.push();console.log(a);a.set({name:$("#name").val(),message:$("#message").val(),md5Email:md5($("#email").val()),postedAt:Firebase.ServerValue.TIMESTAMP});$("input[type=text], textarea").val("");return false})});function slugify(text){return text.toString().toLowerCase().trim().replace(/&/g,"-and-").replace(/[\s\W-]+/g,"-").replace(/[^a-zA-Z0-9-_]+/g,"")}function escapeHtml(str){var div=document.createElement("div");div.appendChild(document.createTextNode(str));return div.innerHTML}
